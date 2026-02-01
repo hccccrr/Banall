b375ff1d3d71e6aa1b2e8375f1bccfca44beb902
hccccrr <vrajiv830@gmail.com>
2026-02-01 14:39:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN,
     OWNER_ID: parseInt(process.env.OWNER_ID),
     plugins: {
-        root: "plugins/Banall"
+        root: "Banall"
     }
 };
